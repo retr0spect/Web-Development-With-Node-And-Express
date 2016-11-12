90a48aac0ce2d12f986d5ce9e7b6c437a8d68188
retr0spect <aditya.metalhead@gmail.com>
2016-11-12 02:30:40 -0700

    Init project, added express and handlebars

---
+++ meadowlark.js
@@ -0,0 +1,40 @@
+var express = require('express');
+
+var app = express();
+
+// set up handlebars view engine 
+var handlebars = require('express3-handlebars').create({ defaultLayout:'main' }); 
+app.engine('handlebars', handlebars.engine); 
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+    res.type('text/plain');
+    res.send('Meadowlark Travel');
+});
+
+app.get('/about', function(req, res) {
+    res.type('text/plain');
+    res.send('About Meadowlark Travel');
+});
+
+// Custom HTTP page
+app.use(function(req, res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found')
+});
+
+// Custom 500 page
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:'
+               + app.get('port') + '; press Cltr-C to terminate.');
+});
