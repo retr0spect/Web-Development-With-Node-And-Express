192930b41ba7dbc72f0430dea280387f1ba9e5ee
retr0spect <aditya.metalhead@gmail.com>
2016-11-12 02:58:57 -0700

    Completed Chapter 3: Saving Time with Express

--- meadowlark.js
+++ meadowlark.js
@@ -9,29 +9,38 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
+
+var fortunes = [        
+    "Conquer your fears or they will conquer you.",        
+    "Rivers need springs.",        
+    "Do not fear what you don't know.",        
+    "You will have a pleasant surprise.",        
+    "Whenever possible, keep it simple.", 
+];
+
+
 app.get('/', function(req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.render('home');
 });
 
 app.get('/about', function(req, res) {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {fortune: randomFortune});
 });
 
-// Custom HTTP page
+// 404 catch-all handler (middleware) 
 app.use(function(req, res) {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found')
+    res.render('404');
 });
 
-// Custom 500 page
+// 500 error handler (middleware) 
 app.use(function(err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function() {
