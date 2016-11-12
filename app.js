b741a6b92fc1e2670d3111fda950304aa6e66f14
retr0spect <aditya.metalhead@gmail.com>
2016-11-12 14:07:03 -0700

    Completed Chapter 4: Mocha, Chai, Zombie Testing

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+var fortune = require('./lib/fortune.js');
+
 // set up handlebars view engine 
 var handlebars = require('express3-handlebars').create({ defaultLayout:'main' }); 
 app.engine('handlebars', handlebars.engine); 
@@ -11,23 +13,29 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next){        
+    res.locals.showTests = app.get('env') !== 'production' &&                
+        req.query.test === '1';        
+    next();
+});
 
-var fortunes = [        
-    "Conquer your fears or they will conquer you.",        
-    "Rivers need springs.",        
-    "Do not fear what you don't know.",        
-    "You will have a pleasant surprise.",        
-    "Whenever possible, keep it simple.", 
-];
+app.get('/tours/hood-river', function(req, res){        
+    res.render('tours/hood-river'); 
+}); 
 
+app.get('/tours/request-group-rate', function(req, res){        
+    res.render('tours/request-group-rate'); 
+});
 
 app.get('/', function(req, res) {
     res.render('home');
 });
 
 app.get('/about', function(req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
 });
 
 // 404 catch-all handler (middleware) 
